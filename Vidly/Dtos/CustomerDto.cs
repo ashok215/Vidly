a773334e460bedbffc96222388f560f8b2f7b821
Ashok s <Ashok s@DESKTOP-8BDJGBG>
2017-06-05 13:15:36 -0500

    After adding apis

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+
+    
+        
+        public byte MembershipTypeId { get; set; }
+
+
+      
+        //[Min18years]
+        public DateTime? Birthdate { get; set; }
+    }
+}
